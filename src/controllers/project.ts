1f2e8b3c160acd0aab53f22a066e2195edf93ee6
joalega <jalexisgarcia.91@gmail.com>
2022-12-07 10:41:33 -0300

    project

--- src/controllers/project.ts
+++ src/controllers/project.ts
@@ -3,39 +3,39 @@ import { RequestHandler } from 'express';
 const Project = require('../models/project');
 
 export const getProject: RequestHandler = async (req, res) => {
-    const { limit = 10, init = 0 } = req.query;
-    const query = { state: true };
-  
-    const [total, project] = await Promise.all([
-      Project.countDocuments(query),
-      Project.find(query).skip(init).limit(limit),
-    ]);
-  
-    res.status(200).json({
-      total,
-      project,
-    });
-  };
+  const { limit = 10, init = 0 } = req.query;
+  const query = { state: true };
+
+  const [total, project] = await Promise.all([
+    Project.countDocuments(query),
+    Project.find(query).skip(init).limit(limit),
+  ]);
+
+  res.status(200).json({
+    total,
+    project,
+  });
+};
 
 export const createProject: RequestHandler = async (req, res) => {
-    const { name, description, participants, requirements } = req.body;
-    const project = new Project({
-        name,
-        description,
-        participants,
-        requirements,
-    });
-    await project.save();
+  const { name, description, participants, requirements } = req.body;
+  const project = new Project({
+    name,
+    description,
+    participants,
+    requirements,
+  });
+  await project.save();
 
-    res.status(200).send(project);
+  res.status(200).send(project);
 };
 
 export const addStudentToProject: RequestHandler = async (req, res) => {
-    const { userId, projectId } = req.body;
-    const project = await Project.findById(projectId);
-    project.students = [...project.students, userId];
-    await project.save();
+  const { userId, projectId } = req.body;
+  const project = await Project.findById(projectId);
+  project.students = [...project.students, userId];
+  await project.save();
 
-    console.log(project);
-    res.status(200).json(project);
+  console.log(project);
+  res.status(200).json(project);
 };
