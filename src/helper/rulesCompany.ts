b380c06e1d854f8ce59c71708de7cdd39115292e
joalega <jalexisgarcia.91@gmail.com>
2022-12-08 12:59:12 -0300

    add utils/formErros

---
+++ src/helper/rulesCompany.ts
@@ -0,0 +1,41 @@
+import { check } from 'express-validator';
+import { verifyToken } from '../middlewares/authValidator';
+import { validate } from '../middlewares/validator';
+// import { validateEmail } from './dbValidators';
+// import { companyRole} from '../middlewares/rolValidator';
+
+export const rulesCreateUserCompany = [
+  check('name', 'Name is required').not().isEmpty(),
+  check('country', 'Country ir required').not().isEmpty(),
+  // check('email').custom(validateEmail),
+  check('email', 'Email is required').not().isEmpty(),
+  validate,
+];
+
+export const rulesUpdateUserCompany = [
+  check('name', 'Name is required').not().isEmpty(),
+  check('country', 'Country ir required').not().isEmpty(),
+  check('email', 'Email is required').not().isEmpty(),
+  validate,
+];
+
+export const rulesGetUserCompany = [
+  verifyToken,
+  check('rol').custom(isValidRole),
+  validate,
+  companyRole,
+];
+
+export const rulesGetUsersCompany = [
+  verifyToken,
+  check('rol').custom(isValidRole),
+  validate,
+  companyRole,
+];
+
+export const rulesDeleteUsersCompany = [
+  verifyToken,
+  check('rol').custom(isValidRole),
+  validate,
+  companyRole,
+];
--- src/routes/project.ts
+++ src/routes/project.ts
@@ -1,13 +1,26 @@
 import { Router } from 'express';
-import { check } from "express-validator";
-import { validate } from "../middlewares/validator";
-import { addStudentToProject, createProject, getProject,getProjects,deleteProject } from '../controllers/project';
+import { check } from 'express-validator';
+import { validate } from '../middlewares/validator';
+import {
+  addStudentToProject,
+  createProject,
+  getProject,
+  getProjects,
+  deleteProject,
+} from '../controllers/project';
 const router = Router();
 
 router.get('/', getProjects);
-router.get('/:id',getProject)
-router.post('/',[check('name','Name is required').not().isEmpty(),check('description','Description is require').not().isEmpty()], createProject);
+router.get('/:id', getProject);
+router.post(
+  '/',
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check('description', 'Description is require').not().isEmpty(),
+  ],
+  createProject,
+);
 router.put('/', addStudentToProject);
-router.delete('/',deleteProject)
+router.delete('/', deleteProject);
 
 module.exports = router;
