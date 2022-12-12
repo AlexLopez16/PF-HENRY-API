6268b2834b6ed9577a6b6be21b43875620754d4c
sciangulahugo <110142598+sciangulahugo@users.noreply.github.com>
2022-12-12 10:50:41 -0300

    add trycatch and redirect to google

--- src/helper/sendConfirmationEmail.ts
+++ src/helper/sendConfirmationEmail.ts
@@ -12,32 +12,37 @@ const {
 } = process.env;
 
 export const sendConfirmationEmail = async (user: any) => {
-    let transport = nodemailer.createTransport({
-        host: HOST_EMAIL,
-        port: PORT_EMAIL,
-        auth: {
-            user: USER_EMAIL,
-            pass: PASS_EMAIL,
-        },
-    });
+    try {
+        let transport = nodemailer.createTransport({
+            host: HOST_EMAIL,
+            port: PORT_EMAIL,
+            auth: {
+                user: USER_EMAIL,
+                pass: PASS_EMAIL,
+            },
+        });
 
-    // Creamos la url con un jwt.
-    const token = jwt.sign(
-        {
-            email: user.email,
-            rol: user.rol,
-        },
-        process.env.TOKEN_SECRET as string,
-        { expiresIn: '24h' }
-    );
-    const urlConfirm = `${URL}:${PORT}/account/confirm/${token}`;
+        // Creamos la url con un jwt.
+        const token = jwt.sign(
+            {
+                email: user.email,
+                rol: user.rol,
+            },
+            process.env.TOKEN_SECRET as string,
+            { expiresIn: '24h' }
+        );
+        const urlConfirm = `${URL}:${PORT}/account/confirm/${token}`;
 
-    // send mail with defined transport object
-    await transport.sendMail({
-        from: FROM_EMAIL, // sender address
-        to: user.email, // list of receivers
-        subject: 'Please, confirm your email', // Subject line
-        //text: 'Hello world', // plain text body
-        html: `<p>Confirm your email: <a href="${urlConfirm}">Confirm</a></p>`, // html body
-    });
+        // send mail with defined transport object
+        const sendEmail = await transport.sendMail({
+            from: FROM_EMAIL, // sender address
+            to: user.email, // list of receivers
+            subject: 'Please, confirm your email', // Subject line
+            //text: 'Hello world', // plain text body
+            html: `<p>Confirm your email: <a href="${urlConfirm}">Confirm</a></p>`, // html body
+        });
+        console.log(sendEmail);
+    } catch (error: any) {
+        console.log(error.message);
+    }
 };
