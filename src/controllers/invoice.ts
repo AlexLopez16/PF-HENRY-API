7d02e0629563587f236f8a533682117ea8fddb0e
Amparo-E <escobarampii@gmail.com>
2022-12-20 15:49:48 -0300

    merge de desarrollo

--- src/controllers/invoice.ts
+++ src/controllers/invoice.ts
@@ -2,25 +2,25 @@ const Invoice = require('../models/invoice');
 const Company = require('../models/company');
 
 type data = {
-    company?: string
-}
+  company?: string;
+};
 
 export const createInvoice = async (data: data = {}) => {
-    try {
-        const company = await Company.findOne({ email: data.company })
+  try {
+    const company = await Company.findOne({ email: data.company });
 
-        if (!company) throw new Error(`User not found with email: ${data.company}`);
-        data.company = company._id
+    if (!company) throw new Error(`User not found with email: ${data.company}`);
+    data.company = company._id;
 
-        const invoice = new Invoice(data)
-        await invoice.save()
+    const invoice = new Invoice(data);
+    await invoice.save();
 
-        company.invoice = [...company.invoice, invoice._id]
-        company.premium = true
-        await company.save()
+    company.invoice = [...company.invoice, invoice._id];
+    company.premium = true;
+    await company.save();
 
-        return invoice;
-    } catch (error: any) {
-        console.log(error);
-    }
-}
+    return invoice;
+  } catch (error: any) {
+    console.log(error);
+  }
+};
