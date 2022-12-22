52b52066ae2cab14016a41bcc583d801c8cb121a
Silvana-Murillas <110003502+Silvana-Murillas@users.noreply.github.com>
2022-12-22 10:55:50 -0500

    modificacion auth para que google funcione y pequenas correcciones en password

--- src/controllers/password.ts
+++ src/controllers/password.ts
@@ -1,9 +1,10 @@
 import { hash } from "../helpers/hash";
 import { RequestHandler } from "express";
-// import { recuperatePassword } from "../helpers/sendConfirmationEmail";
+import { recuperatePassword } from "../helpers/sendConfirmationEmail";
 import { verifyJwt } from "../helpers/verifyJwt";
 
 import { formatError } from "../utils/formatErros";
+import { jwtGenerator } from "../helpers/jwt";
 const Student = require("../models/student");
 const Company = require("../models/company");
 
@@ -18,15 +19,36 @@ export const password: RequestHandler = async (req, res) => {
     }
     if (!user) throw new Error("Email invalid");
 
-    // recuperatePassword(user);
+    recuperatePassword(user);
 
     res.status(200).json({
       msg: "Email send",
     });
   } catch (error: any) {
     res.status(500).json(formatError(error.message));
+
   }
 };
+
+export const redirectPassword:RequestHandler = async (req, res)=>{
+ try{ 
+  console.log('hola')
+  const {token} = req.params
+  console.log(token)
+  const { email } = verifyJwt(token);
+  let user = await Student.findOne({ email: email });
+  if (!user) {
+    user = await Company.findOne({ email: email });
+  }
+  if (!user) throw new Error("Email invalid");
+  let obj={id:user._id,email:user.email}
+  const tok= jwtGenerator(obj);
+  res.redirect(`http://localhost:5173/recoverPassword?token=${tok}&rol=${user.rol}&verify=${user.verify}&id=${user.id}`)
+}catch(error:any){
+  console.log(error.message)
+}
+
+}
 
 export const modifyPassword: RequestHandler = async (req, res) => {
   try {
--- src/helpers/sendConfirmationEmail.ts
+++ src/helpers/sendConfirmationEmail.ts
@@ -11,9 +11,10 @@ const {
   PORT,
 } = process.env;
 
+
 export const sendConfirmationEmail = async (user: any) => {
   try {
-    let transport = nodemailer.createTransport({
+    let  transport = nodemailer.createTransport({
       host: HOST_EMAIL,
       port: PORT_EMAIL,
       auth: {
@@ -40,3 +41,31 @@ export const sendConfirmationEmail = async (user: any) => {
     console.log(error.message);
   }
 };
+
+export const recuperatePassword = async (user: any) => {
+  try {
+    let  transport = nodemailer.createTransport({
+      host: HOST_EMAIL,
+      port: PORT_EMAIL,
+      auth: {
+        user: USER_EMAIL,
+        pass: PASS_EMAIL,
+      },
+    });
+    let obj = { email: user.email};
+
+    // Creamos la url con un jwt.
+    const token = jwtGenerator(obj);
+    const urlmodifyPassword = `${URL}:${PORT}/api/recover/password/redirect/${token}`;
+    // send mail with defined transport object
+    const sendEmail = await transport.sendMail({
+      from: FROM_EMAIL, // sender address
+      to: user.email, // list of receivers
+      subject: "Please,ingrese al link", // Subject line
+      //text: 'Hello world', // plain text body
+      html: `<p>ingrese al link: <a href="${urlmodifyPassword}">Recuperar contraseña</a></p>`, // html body
+    });
+    console.log(sendEmail);
+  } catch (error: any) {
+    console.log(error.message);
+  }}
