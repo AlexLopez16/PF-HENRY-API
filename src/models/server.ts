2a6294e5d6db6949514654451d1d362d3ab64990
ignacio-nicoletti <nicolettiignacio5@gmail.com>
2022-12-30 10:48:14 -0300

    routes admin

--- src/models/server.ts
+++ src/models/server.ts
@@ -33,14 +33,14 @@ const paths = {
 //Routes
 server.use(paths.student, require('../routes/student'));
 server.use(paths.company, require('../routes/company'));
-server.use(paths.auth, require('../routes/auth'));
-server.use(paths.email, require('../routes/email'));
+server.use(paths.auth,    require('../routes/auth'));
+server.use(paths.email,   require('../routes/email'));
 server.use(paths.project, require('../routes/project'));
-server.use(paths.admin, require('../routes/admin'));
-server.use(paths.password, require('../routes/password'));
-server.use(paths.checkout, require('../routes/checkout'));
-server.use(paths.review, require('../routes/review'));
-server.use(paths.token, require('../routes/token'));
+server.use(paths.admin,   require('../routes/admin'));
+server.use(paths.password,require('../routes/password'));
+server.use(paths.checkout,require('../routes/checkout'));
+server.use(paths.review,  require('../routes/review'));
+server.use(paths.token,   require('../routes/token'));
 //DB Connection
 connDB();
 
