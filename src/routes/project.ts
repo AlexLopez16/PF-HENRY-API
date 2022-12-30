fe629efb47af0fde45c047e412f0a00a6d40bb58
br1an17 <106201381+br1an17@users.noreply.github.com>
2022-12-30 11:17:20 -0300

    Revert "Sw"

--- src/routes/project.ts
+++ src/routes/project.ts
@@ -1,32 +1,38 @@
-import { Router } from 'express';
+import { Router } from "express";
 import {
-    addStudentToProject,
-    createProject,
-    getProject,
-    getProjects,
-    deleteProject,
-    editProject,
-    getCategory,
-    addStudentToAccepts,
-    removeStudentToAccepts,
-    unApplyStudent,
-} from '../controllers/project';
+  addStudentToProject,
+  createProject,
+  getProject,
+  getProjects,
+  deleteProject,
+  editProject,
+  getCategory,
+  acceptStudentToProject,
+  DeleteAccepts,
+  UnapplyStudent,
 
-import { rulesCreateProject, rulesProject } from '../helpers/rulesProjects';
-import { verifyToken } from '../middlewares/authValidator';
+} from "../controllers/project";
+
+import {
+  rulesCreateProject,
+  rulesProject,
+} from "../helpers/rulesProjects";
+import { verifyToken } from "../middlewares/authValidator";
 const router = Router();
 
-router.get('/', verifyToken, getProjects);
-router.get('/category', verifyToken, getCategory);
-router.get('/:id', verifyToken, getProject);
+router.get("/", verifyToken, getProjects);
+router.get("/category", verifyToken,getCategory);
+router.get("/:id", verifyToken, getProject);
 
-router.post('/', rulesCreateProject, createProject);
-router.put('/:id', verifyToken, addStudentToProject); //agregar validator rol-student
-router.put('/edit/:id', rulesProject, editProject);
-router.put('/accept/:id', addStudentToAccepts);
-router.put('/denied/:id', removeStudentToAccepts);
-router.put('/unapply/:id', unApplyStudent);
 
-router.delete('/:id', rulesProject, deleteProject);
+router.post("/", rulesCreateProject, createProject);
+router.put("/:id", verifyToken, addStudentToProject);//agregar validator rol-student
+router.put("/edit/:id", rulesProject, editProject);
+router.put('/accept/:id',acceptStudentToProject)
+router.put("/denied/:id",DeleteAccepts)
+router.put('/unapply/:id',UnapplyStudent)
+
+router.delete("/:id", rulesProject, deleteProject);
+
 
 module.exports = router;
