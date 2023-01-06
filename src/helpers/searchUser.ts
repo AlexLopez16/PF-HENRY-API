e30e9870a55795d4392870bd104fde14cae30271
sciangulahugo <110142598+sciangulahugo@users.noreply.github.com>
2023-01-06 05:08:51 -0300

    Refactor resend email

--- src/helpers/searchUser.ts
+++ src/helpers/searchUser.ts
@@ -1,3 +1,5 @@
+import { sendConfirmationEmail } from './sendConfirmationEmail';
+
 const Company = require('../models/company');
 const Student = require('../models/student');
 const Admin = require('../models/admin');
@@ -11,4 +13,36 @@ export const searchUser = async (id: string) => {
         user = await Admin.findById(id);
     }
     return user;
+};
+
+export const searchUserForVerify = async (
+    who: string | any,
+    _id: string,
+    email: string
+) => {
+    try {
+        const functions: object | any = {
+            Student: async (_id: object | any) => {
+                return await Student.findOne({ _id });
+            },
+            Company: async (_id: object | any) => {
+                return await Company.findOne({ _id });
+            },
+            Admin: async (_id: object | any) => {
+                return await Admin.findOne({ _id });
+            },
+        };
+        const user = functions[who];
+        if (user) {
+            if (user.email === email) await sendConfirmationEmail(user);
+            else {
+                user.email = email;
+                await user.save();
+                await sendConfirmationEmail(user);
+            }
+        } else throw new Error('Usuario no encontrado.');
+        return user;
+    } catch (error: any) {
+        console.log(error.message);
+    }
 };
