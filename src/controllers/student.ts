b74cced2fe000a999e9ca00c2f770cf8c71ad781
sciangulahugo <110142598+sciangulahugo@users.noreply.github.com>
2023-01-09 15:14:32 -0300

    x

--- src/controllers/student.ts
+++ src/controllers/student.ts
@@ -32,11 +32,7 @@ export const createStudent: RequestHandler = async (req, res) => {
         });
         user = await user.save();
         // Aca llamamos a la funcion de confirmationEmail.
-<<<<<<< HEAD
         sendConfirmationEmail(user);
-=======
-        await sendConfirmationEmail(user);
->>>>>>> 2f14e1d009b42a0556a8ee62a041bb9d55efd073
         // Solucionado los problemas.
         let rol = user.rol;
         let verify = user.verify;
