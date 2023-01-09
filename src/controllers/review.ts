42141518dfef956fcd443e85b217db7ef4656df4
ignacio-nicoletti <nicolettiignacio5@gmail.com>
2023-01-09 04:18:05 -0300

    reviewAdmin

--- src/controllers/review.ts
+++ src/controllers/review.ts
@@ -1,4 +1,5 @@
 import { RequestHandler } from 'express';
+import { sendMailCancelRating } from '../helpers/sendConfirmationEmail';
 import { formatError } from '../utils/formatErros';
 const Review = require('../models/review');
 const Project = require('../models/project');
@@ -84,33 +85,31 @@ export const editReview: RequestHandler = async (req, res) => {
   }
 };
 
-export const deleteReview: RequestHandler = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const review = await Review.deleteOne({ _id: id });
-
-    res.status(200).send('Review borrado con exito');
-  } catch (error: any) {
-    res.status(500).json(formatError(error.message));
-  }
-};
-
-
 export const getReviews: RequestHandler = async (req, res) => {
   try {
-
-
     let getreviews = await Review.find({})
-      .populate("student", "name")
+      .populate("student", "name email")
       .populate({
         path: "project",
         populate: {
           path: "company",
-          select: "name"
+          select: "name "
         }
       })
     res.status(200).send(getreviews);
   } catch (error) {
     console.log(error);
+  }
+};
+
+export const deleteReview: RequestHandler = async (req, res) => {
+  try {
+    const { idrev, values } = req.body;
+    let review = await Review.findById(idrev).populate("project","name").populate("student","email")
+    await Review.deleteOne({ _id: idrev });
+    sendMailCancelRating(review, values)
+    res.status(200).send('Review borrado con exito');
+  } catch (error: any) {
+    res.status(500).json(formatError(error.message));
   }
 };
