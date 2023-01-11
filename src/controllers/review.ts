9990a4d2a4f35d4c6135378a4543c5454cbeab33
Silvana-Murillas <110003502+Silvana-Murillas@users.noreply.github.com>
2023-01-11 00:05:09 -0500

    filtros reviews en back

--- src/controllers/review.ts
+++ src/controllers/review.ts
@@ -92,17 +92,388 @@ export const editReview: RequestHandler = async (req, res) => {
 
 export const getReviews: RequestHandler = async (req, res) => {
     try {
-        let getreviews = await Review.find({})
-            .populate('student', 'name email')
-            .populate({
-                path: 'project',
-                populate: {
-                    path: 'company',
-                    select: 'name ',
+        const { limit = 6, init = 6, name }: any = req.query;
+        console.log('llego', name);
+        let Limit = parseInt(limit);
+        let Init = parseInt(init);
+
+        if (name) {
+            const count = [
+                {
+                    $lookup: {
+                        from: 'projects',
+                        localField: 'project',
+                        foreignField: '_id',
+                        let: { name: '$name' },
+                        pipeline: [
+                            {
+                                $match: {
+                                    name: {
+                                        $regex: name,
+                                        $options: 'i',
+                                    },
+                                },
+                            },
+                            { $project: { name: 1, company: 1 } },
+                            {
+                                $lookup: {
+                                    from: 'companies',
+                                    localField: 'company',
+                                    foreignField: '_id',
+                                    pipeline: [{ $project: { name: 1 } }],
+                                    as: 'company',
+                                },
+                            },
+                        ],
+                        as: 'project',
+                    },
                 },
-            });
-        res.status(200).send(getreviews);
+                {
+                    $match: { project: { $size: 1 } },
+                },
+                {
+                    $lookup: {
+                        from: 'students',
+                        localField: 'student',
+                        foreignField: '_id',
+                        pipeline: [{ $project: { name: 1 } }],
+                        as: 'student',
+                    },
+                },
+                {
+                    $unionWith: {
+                        coll: 'reviews',
+                        pipeline: [
+                            {
+                                $lookup: {
+                                    from: 'students',
+                                    localField: 'student',
+                                    foreignField: '_id',
+                                    let: { name: '$name' },
+                                    pipeline: [
+                                        {
+                                            $match: {
+                                                name: {
+                                                    $regex: name,
+                                                    $options: 'i',
+                                                },
+                                            },
+                                        },
+                                        { $project: { name: 1 } },
+                                    ],
+                                    as: 'student',
+                                },
+                            },
+                            {
+                                $lookup: {
+                                    from: 'projects',
+                                    localField: 'project',
+                                    foreignField: '_id',
+                                    pipeline: [
+                                        {
+                                            $lookup: {
+                                                from: 'companies',
+                                                localField: 'company',
+                                                foreignField: '_id',
+                                                pipeline: [
+                                                    { $project: { name: 1 } },
+                                                ],
+                                                as: 'company',
+                                            },
+                                        },
+                                        { $project: { name: 1, company: 1 } },
+                                    ],
+                                    as: 'project',
+                                },
+                            },
+                            {
+                                $match: { student: { $size: 1 } },
+                            },
+                        ],
+                    },
+                },
+                {
+                    $unionWith: {
+                        coll: 'reviews',
+                        pipeline: [
+                            {
+                                $lookup: {
+                                    from: 'projects',
+                                    localField: 'project',
+                                    foreignField: '_id',
+                                    pipeline: [
+                                        {
+                                            $lookup: {
+                                                from: 'companies',
+                                                localField: 'company',
+                                                foreignField: '_id',
+                                                let: { name: '$name' },
+                                                pipeline: [
+                                                    {
+                                                        $match: {
+                                                            name: {
+                                                                $regex: name,
+                                                                $options: 'i',
+                                                            },
+                                                        },
+                                                    },
+                                                    { $project: { name: 1 } },
+                                                ],
+                                                as: 'company',
+                                            },
+                                        },
+                                        {
+                                            $match: { company: { $size: 1 } },
+                                        },
+                                        { $project: { name: 1, company: 1 } },
+                                    ],
+                                    as: 'project',
+                                },
+                            },
+                            {
+                                $lookup: {
+                                    from: 'students',
+                                    localField: 'student',
+                                    foreignField: '_id',
+                                    pipeline: [{ $project: { name: 1 } }],
+                                    as: 'student',
+                                },
+                            },
+                            {
+                                $match: { project: { $size: 1 } },
+                            },
+                        ],
+                    },
+                },
+                {
+                    $group: {
+                        _id: '$_id',
+                        description: { $first: '$description' },
+                        project: { $first: '$project' },
+                        student: { $first: '$student' },
+                        ratingCompany: { $first: '$ratingCompany' },
+                        ratingProject: { $first: '$ratingProject' },
+                    },
+                },
+
+                {
+                    $count: 'count',
+                },
+            ];
+            const result1 = [
+                {
+                    $lookup: {
+                        from: 'projects',
+                        localField: 'project',
+                        foreignField: '_id',
+                        let: { name: '$name' },
+                        pipeline: [
+                            {
+                                $match: {
+                                    name: {
+                                        $regex: name,
+                                        $options: 'i',
+                                    },
+                                },
+                            },
+                            { $project: { name: 1, company: 1 } },
+                            {
+                                $lookup: {
+                                    from: 'companies',
+                                    localField: 'company',
+                                    foreignField: '_id',
+                                    pipeline: [{ $project: { name: 1 } }],
+                                    as: 'company',
+                                },
+                            },
+                        ],
+                        as: 'project',
+                    },
+                },
+                {
+                    $match: { project: { $size: 1 } },
+                },
+                {
+                    $lookup: {
+                        from: 'students',
+                        localField: 'student',
+                        foreignField: '_id',
+                        pipeline: [{ $project: { name: 1 } }],
+                        as: 'student',
+                    },
+                },
+
+                {
+                    $unionWith: {
+                        coll: 'reviews',
+                        pipeline: [
+                            {
+                                $lookup: {
+                                    from: 'students',
+                                    localField: 'student',
+                                    foreignField: '_id',
+                                    let: { name: '$name' },
+                                    pipeline: [
+                                        {
+                                            $match: {
+                                                name: {
+                                                    $regex: name,
+                                                    $options: 'i',
+                                                },
+                                            },
+                                        },
+                                        { $project: { name: 1 } },
+                                    ],
+                                    as: 'student',
+                                },
+                            },
+                            {
+                                $lookup: {
+                                    from: 'projects',
+                                    localField: 'project',
+                                    foreignField: '_id',
+                                    pipeline: [
+                                        {
+                                            $lookup: {
+                                                from: 'companies',
+                                                localField: 'company',
+                                                foreignField: '_id',
+                                                pipeline: [
+                                                    { $project: { name: 1 } },
+                                                ],
+                                                as: 'company',
+                                            },
+                                        },
+                                        { $project: { name: 1, company: 1 } },
+                                    ],
+                                    as: 'project',
+                                },
+                            },
+                            {
+                                $match: { student: { $size: 1 } },
+                            },
+                        ],
+                    },
+                },
+                {
+                    $unionWith: {
+                        coll: 'reviews',
+                        pipeline: [
+                            {
+                                $lookup: {
+                                    from: 'projects',
+                                    localField: 'project',
+                                    foreignField: '_id',
+                                    pipeline: [
+                                        {
+                                            $lookup: {
+                                                from: 'companies',
+                                                localField: 'company',
+                                                foreignField: '_id',
+                                                let: { name: '$name' },
+                                                pipeline: [
+                                                    {
+                                                        $match: {
+                                                            name: {
+                                                                $regex: name,
+                                                                $options: 'i',
+                                                            },
+                                                        },
+                                                    },
+                                                    { $project: { name: 1 } },
+                                                ],
+                                                as: 'company',
+                                            },
+                                        },
+                                        {
+                                            $match: { company: { $size: 1 } },
+                                        },
+                                        { $project: { name: 1, company: 1 } },
+                                    ],
+                                    as: 'project',
+                                },
+                            },
+                            {
+                                $lookup: {
+                                    from: 'students',
+                                    localField: 'student',
+                                    foreignField: '_id',
+                                    pipeline: [{ $project: { name: 1 } }],
+                                    as: 'student',
+                                },
+                            },
+                            {
+                                $match: { project: { $size: 1 } },
+                            },
+                        ],
+                    },
+                },
+                {
+                    $group: {
+                        _id: '$_id',
+                        description: { $first: '$description' },
+                        project: { $first: '$project' },
+                        student: { $first: '$student' },
+                        ratingCompany: { $first: '$ratingCompany' },
+                        ratingProject: { $first: '$ratingProject' },
+                    },
+                },
+            ];
+            // const result2 = await Review.aggregate(result1);
+            // let total;
+            const [total, getreviews] = await Promise.all([
+                Review.aggregate(count),
+
+                Review.aggregate(result1).limit(Limit).skip(Init),
+            ]);
+            console.log(total);
+            let tol;
+            if (total[0]) {
+                tol = total[0].count;
+            } else {
+                tol = 0;
+            }
+            // let set;
+            // let array = result2;
+
+            //   if(result2.length>1){
+            //    set=new Set (result2)
+            //    total=set.values.length
+            //    array=Array.from(set)
+            //   }else{
+            //     total=count
+            //   }
+            // if (result3.length > 1) {
+            //     total = 1 + (result2 - result3.length);
+            // } else {
+            //     total = count;
+            // }
+            // const result3 = await Review.aggregate(count);
+            return res.status(200).send({ total: tol, getreviews });
+        }
+        let [total, getreviews] = await Promise.all([
+            await Review.countDocuments({}),
+            await Review.find({})
+                .limit(limit)
+                .skip(init)
+
+                .populate({
+                    path: 'student',
+                    select: 'name email',
+                })
+                .populate({
+                    path: 'project',
+
+                    populate: {
+                        path: 'company',
+                        select: 'name ',
+                    },
+                }),
+        ]);
+        console.log(getreviews);
+        res.status(200).send({ total, getreviews });
     } catch (error) {
+        res.status(500).send({ error: 'sss' });
         console.log(error);
     }
 };
